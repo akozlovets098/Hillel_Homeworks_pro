705c2146d8c18fe2bfbea9e66537f284b75a5ac6
akozlovets098 <an.kozlovets@gmail.com>
2022-04-22 14:25:01 +0300

    Homework on flask

--- h4_flask.py
+++ h4_flask.py
@@ -11,23 +11,23 @@ def hello():
 
 @app.route('/list')
 def show_full_list():
-    with open('h3_textfile') as f:
+    with open('h4_textfile') as f:
         lines = f.readlines()
-    return render_template('h3_template_general.html', lines=lines)
+    return render_template('h4_template_general.html', lines=lines)
 
 
 @app.route('/filter/<word>')
 def show_lines_with_word(word):
-    with open('h3_textfile') as f:
+    with open('h4_textfile') as f:
         lines = [line for line in f.readlines() if word in line]
-    return render_template('h3_filter_list.html', lines=lines, word=word)
+    return render_template('h4_filter_list.html', lines=lines, word=word)
 
 
 @app.route('/show/<int:number>')
 def show_first_lines(number):
-    with open('h3_textfile') as f:
+    with open('h4_textfile') as f:
         lines = f.readlines()[:number]
-    return render_template('h3_list_first_lines.html', lines=lines, number=number)
+    return render_template('h4_list_first_lines.html', lines=lines, number=number)
 
 
 if __name__ == '__main__':
